6274a233ab594ef0094a10280ea77bf485978e93
Maciej Fleischer <fleischer.maciek@wp.pl>
2025-03-14 20:53:41 +0100

    Delete bazaDanych directory

--- bazaDanych/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.bazadanych {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.bazadanych to javafx.fxml;
-    exports com.example.bazadanych;
-}
